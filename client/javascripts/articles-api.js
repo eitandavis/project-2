8fa42d645d856eed5e69f1c9517716bdc37edfc3
Eitan Davis <edavis9@uoregon.edu>
2020-04-16 10:49:29 -0700

    Complete lab 3

--- client/javascripts/articles-api.js
+++ client/javascripts/articles-api.js
@@ -1,19 +1,45 @@
 // jshint esversion: 6
 
-let controller = function(){
+let main = function(){
 
+/*
     let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
     let flickrTag = $("input").???();
-    let requestURL = prefixURL + flickrTag + suffixURL;
+    */
 
+    let requestURL = "https://thejsway-server.herokuapp.com/api/articles";
+
+/*
     //clear old photos
     $(".photos").???("");
+*/
 
-  $.getJSON(requestURL, function(flickrResponse) {
-    flickrResponse.items.forEach(function(item, index) {
+  $.getJSON(requestURL, function(response) {
 
+      let firstArticle = response[0];
+      console.log(`Article ${firstArticle.id}
+      Title: ${firstArticle.title}
+      Content: ${firstArticle.content}`);
+
+    response.forEach(function(item, index) {
+        let newDiv = document.createElement("div");
+        document.querySelector(".b").appendChild(newDiv);
+
+        let header = document.createElement("h3");
+        header.textContent = `Article ${item.id}`;
+        newDiv.appendChild(header);
+
+        let title = document.createElement("p");
+        title.textContent = item.title;
+        newDiv.appendChild(title);
+
+        let content = document.createElement("p");
+        content.textContent = item.content;
+        newDiv.appendChild(content);
+
+/*
       //Flickr returns 20 images by default
       //We need only six images for the Gallery
       if (index < 6) {
@@ -31,10 +57,11 @@ let controller = function(){
         $(".photos").append($img);
         $img.fadeIn();
       }
+      */
     });
-
   });
 };
+
 
 
 $(document).ready(main);
